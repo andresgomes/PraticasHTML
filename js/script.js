d8c229fa4e7259aba5532f667f19b53af36f5b9d
Andre Gomes <andre_colossus@yahoo.com.br>
2016-09-20 14:59:04 -0300

    190916 adicao do bootstrap

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-alert("Olá HCODE!");
+//alert("Olá HCODE!");
+
+var a = 10;
+var b = 33;
+
+function somar(a, b){
+
+	return (a + a);
+}
+
+console.log(somar(2,5));
+document.getElementById("btn-calcular").onclick = function(){
+	
+	var valorA = document.getElementById("valor-a").value;
+	var valorB = document.getElementById("valor-b").value;
+
+		console.log(parseInt(valorA) + parseInt(valorB));
+}
